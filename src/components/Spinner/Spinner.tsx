e50d15bc9a5691db7f3d6540c0edf6ae04dc0b4d
JosueRauda7 <josuerauda.dev@gmail.com>
2025-11-11 05:58:06 -0600

    Mejorar apariencia de loader

--- src/components/Spinner/Spinner.tsx
+++ src/components/Spinner/Spinner.tsx
@@ -1,5 +1,5 @@
 import "./Spinner.css";
 
 export default function Spinner() {
-  return <div className="loader m-5"></div>;
+  return <div className="w-full flex justify-center md:w-lg"><div className="loader m-5"></div></div>;
 }
